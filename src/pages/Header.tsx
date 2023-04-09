6aedec3c4c993b9880055d0852ae38f080ada60e
papa-github <20775567+papa-github@users.noreply.github.com>
2023-04-09 18:22:15 +0100

    Added responsive css

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -6,33 +6,18 @@ interface HeaderLinkProps {
   page: string;
   title: string
   selected: boolean;
-  position: "first" | "middle" | "last";
 }
 
 
 
-const HeaderLink = ({ page, title, selected, position}: HeaderLinkProps) => {
+const HeaderLink = ({ page, title, selected}: HeaderLinkProps) => {
   let className = selected ? 'headerlink-no-link ' : '';
   className += 'headerlink-title';
 
-  // Set title here. First link will have space after, middle links will have space before and after, last link will have space before
-  // This is to make the dots line up
-  // \u00A0 is a non-breaking space
-  let titleDisplay = title;
-  if (position === "first"){
-    titleDisplay += '\u00A0 |';
-  } else if (position === "last"){
-    titleDisplay = '\u00A0 ' + titleDisplay;
-  } else {
-    titleDisplay = '\u00A0 ' + titleDisplay + '\u00A0 |';
-  }
-
   return (
     <Link to={`/${page}`} className={className}>
-      {titleDisplay}
-      <div className={selected ? 'headerlink-dot-active' : 'headerlink-dot'}>
-        •
-      </div>
+      {title}
+
     </Link>
   );
 };
@@ -64,13 +49,6 @@ const Header = () => {
           page={link.page}
           title={link.title}
           selected={page === link.page}
-          position={
-            index === 0
-              ? "first"
-              : index === links.length - 1
-              ? "last"
-              : "middle"
-          }
         />
       ))}
     </div>
